0faf93748f4304816de2e811bc0f9f773c10388e
Tobias Giess <tobias.giess@gmail.com>
2016-12-06 17:22:40 +0100

    improve output

--- src/ch/fhnw/tvver/PCM2MidConverter.java
+++ src/ch/fhnw/tvver/PCM2MidConverter.java
@@ -18,57 +18,76 @@ import ch.fhnw.ether.media.RenderProgram;
 import ch.fhnw.ether.ui.IPlotable;
 
 public class PCM2MidConverter extends AbstractPCM2MIDI {
-    // Attack herunter schrauben
-    // suspend a decand 2 s
-    // attack 0.015
+	// Attack herunter schrauben
+	// suspend a decand 2 s
+	// attack 0.015
 
-    private static final float A_SUB_CONTRA_OCTAVE_FREQ = 25.5f;
-    private static final int HARMONICS = 2;
+	private static final float A_SUB_CONTRA_OCTAVE_FREQ = 25.5f;
+	private static final int HARMONICS = 2;
 
-    public PCM2MidConverter(File track) throws UnsupportedAudioFileException, IOException, MidiUnavailableException,
-            InvalidMidiDataException, RenderCommandException {
-        super(track, EnumSet.of(Flags.REPORT, Flags.WAVE));
-    }
+	public PCM2MidConverter(File track) throws UnsupportedAudioFileException, IOException, MidiUnavailableException,
+			InvalidMidiDataException, RenderCommandException {
+		super(track, EnumSet.of(Flags.REPORT, Flags.WAVE));
+	}
 
-    @Override
-    protected void initializePipeline(RenderProgram<IAudioRenderTarget> program) {
-        // Window defines the windowing function in order to have a continuous signal if the sample
-        // gets repeated multiple times before and after.
-        FFT fft = new FFT(A_SUB_CONTRA_OCTAVE_FREQ, Window.HANN);
+	@Override
+	protected void initializePipeline(RenderProgram<IAudioRenderTarget> program) {
+		// Window defines the windowing function in order to have a continuous
+		// signal if the sample
+		// gets repeated multiple times before and after.
+		FFT fft = new FFT(A_SUB_CONTRA_OCTAVE_FREQ, Window.HANN);
 
-        HpsPitchDetection hps = new HpsPitchDetection(fft, HARMONICS);
-        OnSetDetection osd = new OnSetDetection(fft, hps);
+		HpsPitchDetection hps = new HpsPitchDetection(fft, HARMONICS);
+		OnSetDetection osd = new OnSetDetection(fft, hps);
 
-        program.addLast(new AutoGain());
-        program.addLast(fft);
-        program.addLast(osd);
-        program.addLast(hps);
-        program.addLast(new Converter(hps));
+		program.addLast(new AutoGain());
+		program.addLast(fft);
+		program.addLast(osd);
+		program.addLast(hps);
+		program.addLast(new Converter(hps));
 
-    }
+	}
 
-    private class Converter extends AbstractRenderCommand<IAudioRenderTarget> implements IPlotable {
+	private class Converter extends AbstractRenderCommand<IAudioRenderTarget> implements IPlotable {
 
-        private HpsPitchDetection hps;
+		private HpsPitchDetection hps;
 
-        public Converter(HpsPitchDetection hps) {
-            this.hps = hps;
-        }
+		public Converter(HpsPitchDetection hps) {
+			this.hps = hps;
+		}
 
-        @Override
-        protected void init(IAudioRenderTarget target) throws RenderCommandException {
-        }
+		@Override
+		protected void init(IAudioRenderTarget target) throws RenderCommandException {
+		}
 
-        @Override
-        protected void run(IAudioRenderTarget target) throws RenderCommandException {
-            PitchDetectionResult lastResult = this.hps.getLastResult();
-            if(lastResult != null && lastResult.isPitched()) {
-                PCM2MidConverter.this.noteOn(lastResult.getMidiNote(), 64);
-                PCM2MidConverter.this.noteOff(lastResult.getMidiNote(), 64);
-            }
-            this.hps.clearResult();
-            
-        }
-        
-    }
+		@Override
+		protected void run(IAudioRenderTarget target) throws RenderCommandException {
+			PitchDetectionResult lastResult = this.hps.getLastResult();
+			if (lastResult != null && lastResult.isPitched()) {
+				PCM2MidConverter.this.noteOn(lastResult.getMidiNote(), 64);
+				if(this.getActualNote() == lastResult.getMidiNote()){
+					System.out.println(String.format("%7d%7d%20f%20f", this.getActualNote(), lastResult.getMidiNote(),
+							lastResult.getFreq(), lastResult.getAmplitude()));
+				} else {
+					System.err.println(String.format("%7d%7d%20f%20f", this.getActualNote(), lastResult.getMidiNote(),
+							lastResult.getFreq(), lastResult.getAmplitude()));
+				}
+			}
+			this.hps.clearResult();
+		}
+
+		private int getActualNote() {
+			int velocity = 0;
+			int maxIndex = 0;
+			int[] velocities = PCM2MidConverter.this.getVelocities();
+			for (int index = 0; index < velocities.length; index++) {
+				if (velocities[index] > velocity) {
+					velocity = velocities[index];
+					maxIndex = index;
+				}
+			}
+			return maxIndex;
+		}
+
+	}
 }
