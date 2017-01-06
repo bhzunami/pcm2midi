dfa9796690afc904367f2159448849446ce9380c
Tobias Giess <tobias.giess@gmail.com>
2017-01-06 11:42:05 +0100

    Some refactoring

--- src/ch/fhnw/tvver/pitchdetection/YinPitchDetection.java
+++ src/ch/fhnw/tvver/pitchdetection/YinPitchDetection.java
@@ -15,12 +15,12 @@ import ch.fhnw.ether.media.RenderCommandException;
  */
 public final class YinPitchDetection extends AbstractRenderCommand<IAudioRenderTarget> implements PitchDetection {
 
-	private static final double THRESHOLD = 0.20;
+	private static final double THRESHOLD = 0.2;
 	private static final int AUDIO_BUFFER_SIZE = 2048;
-	
+
 	private float sampleRate;
 	private float[] audioBuffer;
-	private PitchDetectionResult result;
+	private PitchDetectionResult pitchResult;
 	private boolean recording = false;
 	private int recordCounter = 0;
 
@@ -53,103 +53,118 @@ public final class YinPitchDetection extends AbstractRenderCommand<IAudioRenderT
 	}
 
 	private void runDetection() {
-		this.result = new PitchDetectionResult();
-		float[] difference = this.difference(this.audioBuffer);
-		float[] normalizedDifference = this.cumulativeMeanNormalizedDifference(difference);
-		int index = this.absoluteThreshold(normalizedDifference);
-		if (index >= 0) {	
-			float correctedIndex = this.parabolicInterpolation(normalizedDifference, index);
+		this.pitchResult = new PitchDetectionResult();
+		float[] resultBuffer = new float[AUDIO_BUFFER_SIZE / 2];
+		this.applyDifferenceFunction(this.audioBuffer, resultBuffer);
+		this.applyCumulativeMeanNormalization(resultBuffer);
+		int index = this.applyAbsoluteThreshold(resultBuffer);
+		if (index >= 0) {
+			float correctedIndex = this.applyParabolicInterpolation(resultBuffer, index);
 			float freq = this.sampleRate / correctedIndex;
-			this.result.setMidiNote(MidiNoteUtils.getNearestMidiNote(freq));
+			this.pitchResult.setMidiNote(MidiNoteUtils.getNearestMidiNote(freq));
 		} else {
-			this.result.setPitched(false);
+			this.pitchResult.setPitched(false);
 		}
 	}
 
-	private float[] difference(final float[] audioBuffer) {
-		float[] difference = new float[AUDIO_BUFFER_SIZE / 2];
-		for (int offset = 1; offset < difference.length; offset++) {
-			for (int index = 0; index < difference.length; index++) {
+	/**
+	 * Applies the difference function from the Yin paper.
+	 * 
+	 * @param audioBuffer
+	 *            the input buffer
+	 * @param resultBuffer
+	 *            the result buffer
+	 */
+	private void applyDifferenceFunction(float[] audioBuffer, float[] resultBuffer) {
+		for (int offset = 1; offset < resultBuffer.length; offset++) {
+			for (int index = 0; index < resultBuffer.length; index++) {
 				float delta = audioBuffer[index] - audioBuffer[index + offset];
-				difference[offset] += delta * delta;
+				resultBuffer[offset] += delta * delta;
 			}
 		}
-		return difference;
 	}
 
-	private float[] cumulativeMeanNormalizedDifference(float[] difference) {
-		difference[0] = 1;
+	/**
+	 * Applies the cumulative mean normalization on the given buffer
+	 * 
+	 * @param resultBuffer
+	 *            the buffer where this function should be applied
+	 */
+	private void applyCumulativeMeanNormalization(float[] resultBuffer) {
+		resultBuffer[0] = 1;
 		float sum = 0;
-		for (int index = 1; index < difference.length; index++) {
-			sum += difference[index];
-			difference[index] *= index / sum;
+		for (int index = 1; index < resultBuffer.length; index++) {
+			sum = sum + resultBuffer[index];
+			resultBuffer[index] = resultBuffer[index] * index / sum;
 		}
-		return difference;
 	}
 
-	private int absoluteThreshold(float[] normalizedDifference) {
-		int index = 2;
-		for (; index < normalizedDifference.length; index++) {
-			if (normalizedDifference[index] < THRESHOLD) {
-				while (index + 1 < normalizedDifference.length
-						&& normalizedDifference[index + 1] < normalizedDifference[index]) {
+	/**
+	 * Applies the absolute threshold step from the Yin paper. It searches the
+	 * index where the first local minimum exists.
+	 * 
+	 * @param resultBuffer
+	 *            the buffer where to search.
+	 * @return the index of the first local minimum, or -1 of none found.
+	 */
+	private int applyAbsoluteThreshold(float[] resultBuffer) {
+		int index;
+		for (index = 2; index < resultBuffer.length; index++) {
+			if (resultBuffer[index] < THRESHOLD) {
+				while (index + 1 < resultBuffer.length && resultBuffer[index + 1] < resultBuffer[index]) {
 					index++;
 				}
 				break;
 			}
 		}
-		if (index == normalizedDifference.length || normalizedDifference[index] >= THRESHOLD) {
-			this.result.setPitched(false);
+		if (index == resultBuffer.length || resultBuffer[index] >= THRESHOLD) {
+			this.pitchResult.setPitched(false);
 			return -1;
 		} else {
-			this.result.setPitched(true);
+			this.pitchResult.setPitched(true);
 			return index;
 		}
 	}
 
-	private float parabolicInterpolation(float[] normalizedDifference, int index) {
-		float correctedIndex;
-		int x0;
-		int x2;
+	private float applyParabolicInterpolation(float[] resultBuffer, int index) {
+		int indexBelow;
+		int indexAbove;
 		if (index < 1) {
-			x0 = index;
+			indexBelow = index;
 		} else {
-			x0 = index - 1;
+			indexBelow = index - 1;
 		}
-		if (index + 1 < normalizedDifference.length) {
-			x2 = index + 1;
+		if (index + 1 < resultBuffer.length) {
+			indexAbove = index + 1;
 		} else {
-			x2 = index;
+			indexAbove = index;
 		}
-		if (x0 == index) {
-			if (normalizedDifference[index] <= normalizedDifference[x2]) {
-				correctedIndex = index;
+		if (indexBelow == index) {
+			if (resultBuffer[index] <= resultBuffer[indexAbove]) {
+				return index;
 			} else {
-				correctedIndex = x2;
+				return indexAbove;
 			}
-		} else if (x2 == index) {
-			if (normalizedDifference[index] <= normalizedDifference[x0]) {
-				correctedIndex = index;
+		} else if (indexAbove == index) {
+			if (resultBuffer[index] <= resultBuffer[indexBelow]) {
+				return index;
 			} else {
-				correctedIndex = x0;
+				return indexBelow;
 			}
 		} else {
-			float d0 = normalizedDifference[x0];
-			float d1 = normalizedDifference[index];
-			float d2 = normalizedDifference[x2];
-			correctedIndex = index + (d2 - d0) / (2 * (2 * d1 - d2 - d0));
+			return index + (resultBuffer[indexAbove] - resultBuffer[indexBelow])
+					/ (2 * (2 * resultBuffer[index] - resultBuffer[indexAbove] - resultBuffer[indexBelow]));
 		}
-		return correctedIndex;
 	}
 
 	@Override
 	public PitchDetectionResult getResult() {
-		return this.result;
+		return this.pitchResult;
 	}
 
 	@Override
 	public void clearResult() {
-		this.result = null;
+		this.pitchResult = null;
 	}
 
 }
